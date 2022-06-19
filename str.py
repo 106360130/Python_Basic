8ef78fe432325461d8756d60b0a3abbcd8b3e02a
Jakob <jakob1519.ee10@nycu.edu.tw>
2022-06-19 23:05:15 +0800

    String processing

--- str.py
+++ str.py
@@ -15,4 +15,15 @@ print("StrC : {}".format(StrC))
 B = ["Jeff", "Leo", "Jakob"]
 StrD = "".join(B)
 print("StrD : {}".format(StrD))
+
+B = ["Jeff", "Leo", "Jakob"]
+StrE = "".join("{}".format(B))
+print("StrE : {}".format(StrE))
+
+# concatenate to a string and there have a blank between 2 words
+a = [1,2,3]
+StrF = "".join([" {}".format(str(_)) for _ in a])
+print("StrF : {}".format(StrF))
+# concatenate to a string and there have a blank between 2 words
+
 # list to str
